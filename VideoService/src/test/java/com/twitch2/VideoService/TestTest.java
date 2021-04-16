95e29856c06f6e6b2344892893f062e695491bed
manolovisoromero <42765643+manolovisoromero@users.noreply.github.com>
2021-04-17 00:14:28 +0200

    Added test for pipeline

---
+++ VideoService/src/test/java/com/twitch2/VideoService/TestTest.java
@@ -0,0 +1,22 @@
+package com.twitch2.VideoService;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class TestTest {
+
+
+
+    @Test
+    void test_Test() {
+        //Arrange
+        int  actual = 1;
+
+        //Act
+        int expected = 1;
+
+        //Assert
+        Assertions.assertEquals(expected,actual);
+    }
+}
